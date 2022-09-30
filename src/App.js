f6c67c27318c7316002fe2592fcb9a11e52f2d45
baileysurowiec <bailey.surowiec@gmail.com>
2022-09-30 18:59:38 -0500

    UserBar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import UserBar from "./user/UserBar";
 
 function App() {
+  const [name, setName] = useState('');
+  function handleNameChange(evt){
+    setName(evt.target.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name}</h1>
+      <input  type = "text"
+              value = {name}
+              onChange = {handleNameChange}
+      />
+      <UserBar/>
     </div>
   );
 }
---
+++ src/user/Login.js
@@ -0,0 +1,19 @@
+export default function Login(){
+    return(
+        <form       onSubmit = {e => e.preventDefault()}>
+            <label  htmlFor = "login-username"> Username: </label>
+            <input  type = "text"
+                    name = "login-username"
+                    id = "login-username" 
+                    />
+            <label  htmlFor="password"> Password: </label>
+            <input  type = "password"
+                    name = "login-password"
+                    id = "login-password"
+                    />
+            <input  type = "submit"
+                    value = "Login"
+                    />
+        </form>
+    )
+}
